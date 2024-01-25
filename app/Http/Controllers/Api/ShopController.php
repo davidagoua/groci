8a5ab94066cb8cd87b023575a9169480fe0f34cf
mel gibson <christian.agoua@sahanalytics.com>
2024-01-25 21:41:52 +0000

    villes api; propositions;

--- app/Http/Controllers/Api/ShopController.php
+++ app/Http/Controllers/Api/ShopController.php
@@ -68,7 +68,7 @@ class ShopController extends Controller
 
     public function getCategories(Request $request)
     {
-        $categories = Categorie::enfant();
+        $categories = Categorie::query();
         return $this->respondWithSuccess([
             'categories'=> CategorieResource::collection($categories->get())
         ]);
