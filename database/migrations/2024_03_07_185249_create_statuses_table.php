8d28e3e711382ccacecd53373908c01ce65db6aa
cdavidagoua <cdavidagoua@gmail.com>
2024-04-08 12:51:24 +0000

    add barcode column

--- database/migrations/2024_03_07_185249_create_statuses_table.php
+++ database/migrations/2024_03_07_185249_create_statuses_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/*
 class CreateStatusesTable extends Migration
 {
     public function up()
@@ -22,3 +23,4 @@ class CreateStatusesTable extends Migration
         Schema::dropIfExists('statuses');
     }
 }
+*/
