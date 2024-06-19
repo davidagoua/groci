dd5732e02430299ce4f8cecdd03d161581f01644
David Christian Agoua <cdavidagoua@gmail.com>
2024-06-19 16:34:02 +0000

    Update produit_matching.blade.php

--- resources/views/front/shop/produit_matching.blade.php
+++ resources/views/front/shop/produit_matching.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <div class="container-fluid" x-data="{nbCase: 2}">
-        <div class="row"><div class="flex-1"></div><div><button class="btn btn-secondary" @click="nbCase++">Ajouter</button></div></div>
+        <div class="row p-3"><div class="flex-grow-1"></div><div><button class="btn btn-secondary" @click="nbCase++">Ajouter</button></div></div>
         <div class="row">
-            <div x-for="nb in nbCase" class="col-12 col-md-4">
+            <div x-for="i in nbCase" class="col-12 col-md-4">
                 <livewire:compare-basket />
             </div>
       
