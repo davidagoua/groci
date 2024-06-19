6256876faea5667b6c23687d3f45291c9f9cda5e
David Christian Agoua <cdavidagoua@gmail.com>
2024-06-19 16:38:09 +0000

    Update produit_matching.blade.php

--- resources/views/front/shop/produit_matching.blade.php
+++ resources/views/front/shop/produit_matching.blade.php
@@ -6,7 +6,7 @@
         <div class="row p-3"><div class="flex-grow-1"></div><div><button class="btn btn-secondary" @click="nbCase++">Ajouter</button></div></div>
         <div class="row">
             <template x-for="i in nbCase">
-                <div class="col-12 col">
+                <div class="col-12 col-3">
                     <livewire:compare-basket />
                 </div>
             </template>
