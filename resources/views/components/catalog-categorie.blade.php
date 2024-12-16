712b059402ac03f55765bd197b47387e069d7aa9
root <daniel.ntaye@sahanalytics.com>
2024-12-16 22:49:06 +0000

    add button delete all product

--- resources/views/components/catalog-categorie.blade.php
+++ resources/views/components/catalog-categorie.blade.php
@@ -15,11 +15,10 @@
         }finally{
             this.loading = false
         }
-
     }
 }">
     <div class="d-flex row">
-        <div class="col-md-4 border-b border-none border">
+        <div class="col-md-4 border-b border-none border" style="max-height: 80vh; overflow-y: scroll">
             @foreach($categorie->enfants()->get() as $scat)
             <a class="d-block p-1 mt-2 rounded shadow bg-white text-center" :class="{'border border-danger': selectedId === {{$scat->id}}  }" href="#" @click.prevent="getCategorieEnfants({{$scat->id}})">
                <div class="d-flex align-items-center">
@@ -36,10 +35,10 @@
             <div x-show="loading" class="text-center w-full h-full">
                 <span class="mdi mdi-loading mdi-spin"></span>
             </div>
-            <div x-show="enfants.length > 0" >
+            <div x-show="enfants.length > 0" x-transition>
 
                 <template x-for="enfant in enfants" :key="enfant.id">
-                    <a :href="'{{ route('front.shop.search') }}?cats[0]='+enfant.id +'&parent=enfant.parent_id'" class="d-block shadow bg-white rounded align-items-center p-1 d-flex align-items-center ">
+                    <a :href="'{{ route('front.shop.search') }}?cats[0]='+enfant.id +'&parent=enfant.parent_id'" class="d-block mb-1 shadow bg-white rounded align-items-center p-1 d-flex align-items-center ">
                         <div class="d-flex flex-grow-1 align-items-center">
                             <img  style="height: 70px; width: 70px" :src="enfant.image" class="mr-3" alt="" >
                             <span x-text="enfant.nom"></span>
